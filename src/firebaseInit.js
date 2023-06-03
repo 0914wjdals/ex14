a18a6f86567703351a1289b2bb996901092dede0
0914wjdals <1106wjdals@gmail.com>
2023-06-03 11:20:39 +0900

    ver01

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -7,7 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyCA0RVunkSw1BbC2t1mbM3V2eAu-LV_KY0",
+  authDomain: "fir-f8955.firebaseapp.com",
+  databaseURL: "https://fir-f8955-default-rtdb.firebaseio.com/",
+  projectId: "fir-f8955",
+  storageBucket: "fir-f8955.appspot.com",
+  messagingSenderId: "1083294247920",
+  appId: "1:1083294247920:web:9b8b5833a7bef605424b35",
+  measurementId: "G-3LGRSLZ2X2"
 };
 
 // Initialize Firebase
